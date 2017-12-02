370fc0eafb3b4996fc0caf61c1910671353c02ba
Adam Tropp <adt6261@gmail.com>
2017-12-02 22:38:30 +0000

    added ability for user to slug username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   has_many :leagues, through: :teams
   has_many :players, through: :teams
   has_secure_password
+
+  def slug
+    self.username.gsub(" ", '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |username|
+      username.slug == slug
+    end
+  end
 end
