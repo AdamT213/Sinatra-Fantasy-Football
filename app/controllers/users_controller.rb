9f0560763a8deab83115d4c170fdb0c2035d072a
Adam Tropp <adt6261@gmail.com>
2017-12-02 22:28:40 +0000

    added login routes and login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,36 @@ class UsersController < ApplicationController
       redirect to '/leagues'
     end
   end
+
+  get '/login' do
+    if !is_logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/leagues'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params["username"])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/leagues'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/logout' do
+    if is_logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params["slug"])
+    erb :'/users/show'
+  end
 end
