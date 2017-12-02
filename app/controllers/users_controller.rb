2c2268c9101c4eb64a3c4ef0842a6f9a04071a1f
Adam Tropp <adt6261@gmail.com>
2017-12-02 22:00:36 +0000

    changed password field on signup to hidden

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    if !is_logged_in
+    if !is_logged_in?
       erb :'/users/new'
     else
       redirect to '/leagues'
     end
   end
+
+  post '/signup' do
+    @user = User.create(params)
+    @user.save
+    redirect to '/leagues'
+  end
+end
