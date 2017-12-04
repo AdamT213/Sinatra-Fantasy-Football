91ce369f06c6d8810ee61d6f05ea4f417059b105
Adam Tropp <adt6261@gmail.com>
2017-12-04 19:35:36 +0000

    added route and form to create new player

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,39 @@
+class PlayersController < ApplicationController
+  get '/players/new' do
+    if is_logged_in?
+      erb :'/players/create_player'
+    else
+      erb :index
+    end
+  end
+
+  # post '/teams' do
+  #   if !params[:name].empty?
+  #     @team = Team.create(params)
+  #     @team.save
+  #     redirect to "/teams/#{@team.id}"
+  #   else
+  #     redirect to '/teams/new'
+  #   end
+  # end
+  #
+  # get '/teams/:id' do
+  #   @team = Team.find_by_id(params[:id])
+  #   erb :'/teams/show_team'
+  # end
+  #
+  # delete '/teams/:id/delete' do
+  #   if is_logged_in?
+  #     @team = Team.find_by_id(params[:id])
+  #     if @team.user.id == current_user.id
+  #       @team.delete
+  #       @team.league.users.delete(current_user)
+  #       redirect to '/leagues'
+  #     else
+  #       redirect to '/leagues'
+  #     end
+  #   else
+  #     erb :index
+  #   end
+  # end
+end
