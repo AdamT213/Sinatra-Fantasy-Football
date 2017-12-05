d359d6169520ab20f5f7055d85b51680d32f8c5c
Adam Tropp <adt6261@gmail.com>
2017-12-05 02:53:04 +0000

    added league_id to teams to allow viewing of leagues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204232248) do
+ActiveRecord::Schema.define(version: 20171205025007) do
 
   create_table "games", force: :cascade do |t|
     t.string "winning_team_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20171204232248) do
     t.string "name"
     t.integer "wins"
     t.integer "losses"
+    t.integer "league_id"
   end
 
   create_table "users", force: :cascade do |t|
